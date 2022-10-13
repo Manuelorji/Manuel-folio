aa32406e819831317e79c2fb12f8ebf992596c51
Manuelorji <nuelorji@gmail.com>
2022-10-13 18:16:08 +0100

    edited the slider component

--- src/Components/Slider/Slider.js
+++ src/Components/Slider/Slider.js
@@ -28,7 +28,7 @@ export default function Slider({ children, cardData }) {
   //slide
   const slide = (direction) => {
     if (direction === "right") {
-      if (currentIndex !== cardData.length / 3 + 3) {
+      if (currentIndex !== cardData.length / 3 + 5) {
         currentIndex++;
         prevBtn.current.style.opacity = "1";
       } else {
